bb0284251a0efb907a24d263ff842214436b5461
Luiza Bortolotto <Luiza Bortolotto>
2016-09-02 22:20:36 -0300

    programa funcional

--- main.c
+++ main.c
@@ -12,30 +12,46 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-// Declaração da variável numero.
-int numero = 2;
-
-// Declara que o ponteiro é igual à posição da variável numero.
-int *ptr_numero = &numero;
-
 int main(int argc, char *argv[])//esses argumentos nós não utilizamosa, eles já estão no projeto. 
 {
+	// Criação e inicialização das variáveis.
+	int variavel_1 = 1, variavel_2 = 2;
+	char controle = 'n', descarga;
 	
-	//Solicita um número para o usuário.
-	printf("Insira um numero: ");
+	// Criação dos ponteiros.
+	int *ponteiro_1, *ponteiro_2;
 	
-	//Armazena o número inserido no ponteiro. 
-	//scanf("%i", &numero);
-	
-	//Armazena o endereço da variável original.
-	scanf("%i", ptr_numero);
-	
-	//Imprime na telao número inserido.
-	//printf("\nO numero inserido foi: %i!", numero);
-	
-	//Imprime o conteudo da variável pra qual o ponteiro está apontando.
-	printf("\nO numero inserido foi: %i!", *ptr_numero);
-	
+	// Enquanto.
+	while (1)
+	{
+		// Inicialização dos ponteiros.
+		// Ponteiro_1 recebe o endereço da variavel_1.
+		ponteiro_1 = &variavel_1;
+		// Ponteiro_2 recebe o endereço da variavel_2.
+		ponteiro_2 = &variavel_2;
+		
+		// Exibe o valr contido em cada variável a partir do endereço contido 
+		//nos ponteiros.
+		printf("\nvariavel_1 = %i", *ponteiro_1);
+		printf("\nvariavel_2 = %i", *ponteiro_2);
+		
+		// Exibe a solicitação de controle.
+		printf("\n\nDeseja finalizar o programa? (s/n) ");
+		// Efetua a leitura do comando.
+		scanf("%c", &controle);
+		// Se o comando indicar a finalização, finaliza o loop.
+		if (controle == 's') break;
+		
+		// Exibe a solicitação de um valor.
+		printf("\nDigite um valor para a variavel_1: ");
+		// Efetua a leitura do valor e transfere o valor para a variavel_1.
+		scanf("%i", ponteiro_1);
+		// Exibe a solicitação de um valor.
+		printf("Digite um valor para a variavel_2: ");
+		// Efetua a leitura do valor e tranfere o valor para a variavel_2.
+		scanf("%i%c", ponteiro_2, &descarga);
+		
+	}
 	//Retorna zero.
 	return 0;
 }

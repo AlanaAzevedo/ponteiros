9e38ac3aeff5179be94e0c46f6e3817ebff72119
Luiza Bortolotto <Luiza Bortolotto>
2016-09-09 20:45:12 -0300

    programa funcional

--- main.c
+++ main.c
@@ -11,62 +11,27 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void swap (int *i, int *j)
+
+
+int main()
 {
-    int temp;
-    temp = *i;
-    *i = *j;
-    *j = temp;
-}
- 
-int main(int argc, char *argv[])//esses argumentos nós não utilizamosa, eles já estão no projeto. 
-{
-	// Criação e inicialização das variáveis.
-	int variavel_1 = 1, variavel_2 = 2;
-	char controle = 'n', descarga;
+	//criação e inicialização das variáveise ponteiros.
+	//char Minha_String [] = {"1234567890"};
+	char Minha_String [] = {'1','2','3','4','5','6','7','8','9','0'};
+	char *Meu_Ponteiro = &Minha_String;
 	
-	// Criação dos ponteiros.
-	int *ponteiro_1, *ponteiro_2;
-	
-	// Enquanto.
-	while (1)
+	//exibe o valor contido em cada posição da string a partir do endereço
+	//contido no ponteiro.
+	do
 	{
-		// Inicialização dos ponteiros.
-		// Ponteiro_1 recebe o endereço da variavel_1.
-		ponteiro_1 = &variavel_1;
-		// Ponteiro_2 recebe o endereço da variavel_2.
-		ponteiro_2 = &variavel_2;
-		
-		// Exibe o valr contido em cada variável a partir do endereço contido 
-		//nos ponteiros.
-		printf("\nvariavel_1 = %i", *ponteiro_1);
-		printf("\nvariavel_2 = %i", *ponteiro_2);
-		 
-		// Exibe a solicitação de controle.
-		printf("\n\nDeseja finalizar o programa? (s/n) ");
-		// Efetua a leitura do comando.
-		scanf("%c", &controle);
-		// Se o comando indicar a finalização, finaliza o loop.
-		if (controle == 's') break;
-		
-		// Exibe a solicitação de um valor.
-		printf("\nDigite um valor para a variavel_1: ");
-		// Efetua a leitura do valor e transfere o valor para a variavel_1.
-		scanf("%i", ponteiro_1);
-		// Exibe a solicitação de um valor.
-		printf("Digite um valor para a variavel_2: ");
-		// Efetua a leitura do valor e tranfere o valor para a variavel_2.
-		scanf("%i%c", ponteiro_2, &descarga);
-		
-		printf("\nvariavel_1 = %i", variavel_1);
-		printf("\nvariavel_2 = %i", variavel_2);
-   		
-		swap (&variavel_1, &variavel_2);
-    	
-		printf ("\n\nDepois da troca elas valem:");
-		
-   		
+		printf("\nO Meu_Ponteiro aponta para o valor: %c\n", *Meu_Ponteiro);
+		Meu_Ponteiro++;
 	}
-	//Retorna zero.
+	
+	while (*Meu_Ponteiro != 0);
+	
+	printf("\nO tamanho da Minha_String e: %d\n", sizeof (Minha_String));
+	
+	//retorna 0
 	return 0;
 }
